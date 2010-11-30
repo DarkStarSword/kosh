071c5945f6de01a73048e15947431e79799f9d56
Ian Munsie <darkstarsword@gmail.com>
2010-11-30 18:59:09 +1100

    ssh password changer prototype: 1st commit
    I'm working on a script to automate changing passwords over SSH. For the
    moment I'm implementing it in a separate script, but once it works I'll
    look into integrating it into the main password manager.
    
    Actually integrating it will require some additions to the password
    database format - I want to have each machine have it's own entry that
    is updated independently of the password entry. The password entry will
    point at all machines that should have that password, and that machine
    entry will point to the password that it is currently set to:
    
    - We update a password entry (ie, create a new entry with a new timestamp)
    - This entry points to all machine entries that the old entry pointed to.
    - The machine entries still point to the old password entry
    - As the password is changed (and confirmed) on each machine, it is
      updated to point to the new password entry.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

---
+++ sshpasswd.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python
+# vi:sw=2:ts=2:expandtab
+
+from __future__ import print_function
+
+import pexpect
+
+def cprint(colour, msg, sep=' ', end='\n', file=None):
+  ttycolours = {
+      'red'    : '[1;31;40m',
+      'green'  : '[1;32;40m',
+      'yellow' : '[1;33;40m',
+      'reset'  : '[0;37;40m',
+      }
+  try:
+    print(ttycolours[colour] + msg + ttycolours['reset'], sep=sep, end=end, file=file)
+  except KeyError:
+    raise
+
+old_pass_prompts = [
+    'current.*password: ',
+    'Old Password: ',
+    'Enter login\(LDAP\) password: ',
+    '\(current\) UNIX password: ',
+    ]
+
+new_pass_prompts = [
+    'Enter new password: ',
+    'New Password: ',
+    'Enter new UNIX password: ',
+    ]
+
+confirm_pass_prompts = [
+    'Re-type new password: ',
+    'Retype new UNIX password: ',
+    'Re-enter new password: ',
+    'Reenter New Password: ',
+    ]
+
+# TODO: Find more failure prompts
+failure_prompts = [
+    'password unchanged',
+    ]
+
+success_prompts = [
+    'Password changed.',
+    'passwd: password updated successfully',
+    ]
+
+def flatten1(l):
+  """
+  Flattens a list of lists by one level
+     [[a,b,c],[d,e,f],[g,h,[i,j,k]]]
+  => [ a,b,c,  d,e,f,  g,h,[i,j,k]]
+  """
+  return reduce(lambda a,b:a+b, l)
+
+def expect_groups(self, groups):
+  flatten = flatten1(groups)
+  ret = self.expect(flatten)
+  newgroups = flatten1([[g]*len(g) for g in groups])
+  return newgroups[ret]
+
+def test_expect_groups():
+  groups = [old_pass_prompts, new_pass_prompts, confirm_pass_prompts, failure_prompts, success_prompts]
+  for g in groups:
+    for prompt in g:
+      # Replace regular expressions with literals:
+      prompt = reduce(lambda x,(a,b): x.replace(a,b), [('\(','('),('\)',')')], prompt)
+      print("testing response to '%s': "%prompt,end='')
+      self = pexpect.spawn("echo '%s'"%prompt)
+      ret = expect_groups(self, groups)
+      self.close()
+      if ret != g:
+        print("\nfail:'\nMatched %s\nnot     %s\n"%(e,str(ret),str(g)))
+      else:
+        print('ok')
+
+test_expect_groups()
+import sys
+sys.exit(0)
+
+
+def change_tty_passwd(oldpass, newpass, tty=None):
+  if tty is None:
+    cprint('yellow', "Changing local password")
+    tty = pexpect.spawn('passwd')
+  else:
+    tty.prompt()
+    tty.sendline('passwd')
+
+  cprint('yellow','waiting for current password prompt...')
+  # FIXME: Alternate prompts?
+  result = tty.expect(['current.*password: ', 'Old Password: ', pexpect.TIMEOUT, pexpect.EOF])
+  if result >= 2:
+    cprint('red', 'timeout or EOF while waiting for current password prompt')
+    print(tty.before)
+    raise PasswordChangeFailure()
+
+  cprint('yellow','sending old password...')
+  tty.sendline(oldpass)
+
+  result = tty.expect(['Enter new password: ', 'New Password: ', 'password unchanged', pexpect.TIMEOUT, pexpect.EOF])
+  if result >= 2:
+    cprint('red', 'Wrong old password')
+    print(tty.before)
+    raise PasswordChangeFailure()
+
+  # cprint('yellow','sending new password...')
+  # tty.sendline(newpass)
+
+  # result = tty.expect(['Re-type new password: ', 'Reenter New Password: ', 'password unchanged', pexpect.TIMEOUT, pexpect.EOF])
+  # if result >= 2:
+  #   cprint('red', 'Problem with new password')
+  #   print(tty.before)
+  #   raise PasswordChangeFailure() # FIXME - could have changed
+
+  # cprint('yellow','Re-sending new password...')
+  # tty.sendline(newpass)
+
+  # result = tty.expect(['Password changed.', pexpect.TIMEOUT, pexpect.EOF])
+  # if result >= 1:
+  #   cprint('red', 'Confirming new password appears to have failed?')
+  #   print(tty.before)
+  #   raise PasswordChangeFailure() # FIXME - could have changed
+
+  tty.close(True)
+  # FIXME: Look at passwd code for other possible results
+
+class LoginFailure(Exception): pass
+
+def ssh_open(host, username, password = '', force_password = True):
+  import pxssh
+  s = pxssh.pxssh()
+  s.force_password = force_password
+  try:
+    s.login(host, username, password)
+    print(s.before)
+  except pxssh.ExceptionPxssh, e:
+    raise LoginFailure(str(e))
+  except pexpect.EOF, e:
+    raise LoginFailure('EOF: Check the hostname')
+  except pexpect.TIMEOUT, e: raise
+  return s
+
+class PasswordChangeFailure(Exception): pass         # Password was NOT changed
+class PasswordChangeVerifyFailure(Exception): pass   # Password changed, but verification failed
+
+def verify_ssh_passwd(host, username, password):
+  try:
+    s = ssh_open(host, username, password)
+    s.logout()
+    s.close(True)
+    return True
+  except LoginFailure, e:
+    return False
+  except pexpect.TIMEOUT, e: raise
+
+def change_ssh_passwd(host, username, oldpass, newpass):
+  # Login:
+  print('Logging in to %s...'%host, end='')
+  try:
+    s = ssh_open(host, username, oldpass)
+    cprint('green', 'Ok')
+  except LoginFailure, e:
+    cprint('red', 'failure: %s'%str(e))
+    raise PasswordChangeFailure(str(e))
+
+  print('Changing password...', end='')
+  # Change:
+  try:
+    change_tty_passwd(oldpass, newpass, s)
+  except:
+    cprint('red', 'Exception while attempting to change password')
+
+  # Logout:
+  s.logout()
+  s.close(True)
+
+  # Verify:
+  print('Verifying change on %s...'%host, end='')
+  if verify_ssh_passwd(host, username, newpass):
+    cprint('green', ' Success')
+  else:
+    cprint('red', ' Failure')
+    print('Testing old password...', end='')
+    if verify_ssh_passwd(host, username, oldpass):
+      cprint('yellow', ' Password unchanged!')
+      raise PasswordChangeFailure()
+    else:
+      cprint('red', ' Verification Failed!')
+      raise PasswordChangeVerifyFailure()
+
+# Proposed API:
+# change takes: 
+#  machine entry
+#  old password entry
+#  new password entry (or just new password?)
+#  User Interface callbacks
+# change needs to return state:
+#  Definitely not changed
+#  Possibly changed
+#  Definitely changed
+#
+# If verify method is available it will always be called so long as password could have/was changed (maybe always?)
+# verify takes:
+#  machine entry
+#  password entry
+#
+# Probably need to pass some extra state between these as well
+#
+#
+#  User Interface callbacks
+#ChangeMethods = {
+#    'ssh': {'change': change_ssh_passwd,
+#            'verify': test_ssh_passwd}
+#    'config': {'change': change_config}
+#    }
+#
+# Possibly I should also add a login/logout pair...
+#
+#
+# URLs will be a difficult case...
+#  state -> logging in
+#  goto URL
+#  submit form | submit password
+#  state -> logged in | failed
+#  if logged in:
+#   { goto URL || follow link }
+#   state -> changing password
+#   submit form
+#   state -> password possibly changed | password changed | change failed
+#   log out
+# VERIFY:
+#  state -> verifying change
+#  goto URL
+#  submit form | submit password
+#  state -> change succeeded | verification stage failed
+#
+# Actions for:
+#  GOTO URL
+#  Follow link on page (regexp match?)
+#  Fill in form
+#  Submit form
+#
+#
+
+def main():
+  import getpass, sys
+  username = oldpass = newpass = newpass1 = ''
+  while username == '':
+    username = raw_input('username: ')
+  while oldpass == '':
+    oldpass = getpass.getpass('old password: ')
+  #while newpass == '' || newpass != newpass1:
+  #  newpass = getpass.getpass('new password: ')
+  #  newpass1 = getpass.getpass('Confirm new password: ')
+
+  for host in sys.argv[1:]:
+    try:
+      cprint('yellow', 'Verifying password on %s...'%host)
+      if host == 'localhost':
+        change_tty_passwd(oldpass, newpass)
+      else:
+        try:
+          if verify_ssh_passwd(host, username, oldpass):
+            cprint('green', 'Ok')
+          else:
+            cprint('red', 'not ok')
+        except pexpect.TIMEOUT, e:
+          cprint('red', 'timeout')
+    except:
+      cprint('red', 'Exception verifying password on %s'%host)
+
+  # for host in sys.argv[1:]:
+  #   try:
+  #     cprint('yellow', 'Changing password on %s...'%host)
+  #     if host == 'localhost':
+  #       change_tty_passwd(oldpass, newpass)
+  #     else:
+  #       change_ssh_passwd(host, username, oldpass, newpass)
+  #   except:
+  #     cprint('red', 'Password change failed on %s'%host)
+
+if __name__ == '__main__': main()
