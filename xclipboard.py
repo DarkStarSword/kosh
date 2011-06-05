02f230f884bb217a210d1085edddd6ba19e2988d
Ian Munsie <darkstarsword@gmail.com>
2011-06-05 19:51:40 +1000

    X Clipboard: Send subsequent credentials through same selection
    Once we get a selection request and send the first credential, we almost
    always expect subsequent requests to come through the same selection.
    
    This may not be universally true... but for the moment I'm going to only
    allow subsequent requests to go through the same selection anyway. If
    this becomes an issue feel free to revert this patch.
    
    Signed-off-by: Ian Munsie <darkstarsword@gmail.com>

--- xclipboard.py
+++ xclipboard.py
@@ -248,6 +248,9 @@ def sendViaClipboard(blobs, record = None, txtselections = defSelections, ui=ui_
                   # Don't break immediately, transfer will not have finished.
                   # Wait until the property has been deleted by the requestor
                   awaitingCompletion.append((e.requestor, e.property))
+                  # FIXME: Release other selections
+                  selections = [e.selection]
+                  txtselections = [display.get_atom_name(e.selection)]
               elif e.type == X.PropertyNotify:
                 if (e.window, e.atom) in awaitingCompletion \
                     and e.state == 1: # Deleted
@@ -260,8 +263,9 @@ def sendViaClipboard(blobs, record = None, txtselections = defSelections, ui=ui_
                 if e.time == X.CurrentTime or e.time >= timestamp:
                   # If we lost CLIPBOARD (explicit copy) or no longer control any selection, abort:
                   name = display.get_atom_name(e.atom)
-                  selections.remove(e.atom)
-                  txtselections.remove(name)
+                  if e.atom in selections:
+                    selections.remove(e.atom)
+                    txtselections.remove(name)
                   if name == 'CLIPBOARD' or not selections:
                     # If transfer is in progress it should be allowed to complete:
                     if awaitingCompletion == []: return
