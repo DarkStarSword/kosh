494a382af81a92f775714e176ce95ffef6d1e1f8
Ian Munsie <darkstarsword@gmail.com>
2015-05-29 16:39:37 +1000

    Add licensing information
    python-xlib is GPLv2+, everything else is more permissive, so let's go
    with... (rolls dice)... GPLv3+!

--- import.py
+++ import.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python2.6
 # vi:sw=2:ts=2:expandtab
 
+# Copyright (C) 2009-2015 Ian Munsie
+#
+# Kosh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Kosh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kosh.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import sys
 import csv
 
